7f619f0dadb862a30d74c1a4ad7975bc03fbc0e6
Pr0gger1 <kraus2202200491@gmail.com>
2022-07-01 23:06:08 +0300

    Настроена правильная сортировка футбольных команд

--- command_task.py
+++ command_task.py
@@ -1,38 +1,29 @@
+import itertools
 from sys import argv
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
 
-# Сброс ограничений на количество выводимых рядов
-pd.set_option('display.max_rows', None)
 
-# Сброс ограничений на число столбцов
-pd.set_option('display.max_columns', None)
-
-# Сброс ограничений на количество символов в записи
-pd.set_option('display.max_colwidth', None)
+# # Сброс ограничений на количество выводимых рядов
+# pd.set_option('display.max_rows', None)
+#
+# # Сброс ограничений на число столбцов
+# pd.set_option('display.max_columns', None)
+#
+# # Сброс ограничений на количество символов в записи
+# pd.set_option('display.max_colwidth', None)
 
 
 class Football:
-
     def __init__(self, file: str):
         # извлечение таблицы из csv файла
         self.data = pd.read_csv(file)
         # счёт команд
-        self.score = self.data["счёт"].apply(lambda s: list(map(int, s[:3].split(":"))))
-
-        self.extra_score = pd.Series(
-            filter(None, list(map(lambda x: x.split(" ")[-1].replace("*", ""), self.data["счёт"]))))
-        self.extra_score = self.extra_score.apply(self.apply_func)
+        self.score = self.data["счёт"].apply(self.__parsing_score)
 
         # команды, играющие друг против друга
         self.teams = [self.data["Команда_1"], self.data["Команда_2"]]
-
-    def apply_func(self, val):
-        try:
-            return list(map(int, val[1:4].split(":")))
-        except:
-            return [0, 0]
 
     def final_table(self):
         # первичная таблица
@@ -44,38 +35,42 @@ class Football:
         # таблица с вычисленными итоговыми очками
         final_scores: DataFrame = self.__calcultate_final_scores(game_scores)
 
-        extra_scores: DataFrame = self.__calculate_extra_scores(tables=self.teams)
-
         # склеиваем все таблицы в одну
-        table = table.join([game_scores, goals, final_scores, extra_scores])
+        table = table.join([game_scores, goals, final_scores])
 
         table = table.join(DataFrame({
             "Р": table["ЗГ"] - table["ПГ"]
         }))
 
         # сортируем таблицу по результирующим очкам
-        table = table.sort_values(by=["О", "В", "ЗГ", "Р"], ascending=False).reset_index().rename(
+        table = table.sort_values(by=["О"], ascending=False).reset_index().rename(
             columns={"index": "Команда"})
 
-        # переопределяем индекс
-        table.index = np.arange(1, table.shape[0] + 1)
+        # создаем объект с количеством вхождений очков
+        scores = table['О'].value_counts()
 
-        # table = self.__sort_table(table)
+        # если есть команды с равными очками
+        if not scores[scores > 1].empty:
+            # таблица с личными встречами команд
+            private_meetings_table = self.__private_meetings(table)
+            # выполняем слияние основной таблицы с таблицей личных встреч
+            table = pd.merge(table, private_meetings_table, how="outer")
+            # сортируем по столбцам обоих таблиц
+            table = table.sort_values(by=["О", "Выигр", "Заб", "Разн", "В", "ЗГ"], ascending=False)
 
+            # переопределяем индекс
+            table.index = np.arange(1, table.shape[0] + 1)
+
+            # удаляем ненужные столбцы
+            table.drop(columns=["Выигр", "Заб", "Разн"], axis=1, inplace=True)
+
+        # меняем местами колонки
+        table = table.reindex(columns=["Команда", "Игры", "В", "Н", "П", "ЗГ", "ПГ", "Р", "О"])
         return table
 
-    def __calculate_extra_scores(self, tables: list):
-        extra_1 = DataFrame({
-            "Команда": tables[0],
-            "Доп.": self.extra_score.apply(lambda x: x[0])
-        }).groupby("Команда").sum()
-
-        extra_2 = DataFrame({
-            "Команда": tables[1],
-            "Доп.": self.extra_score.apply(lambda x: x[1])
-        }).groupby("Команда").sum()
-
-        return (extra_1.add(extra_2)).astype("int64")
+    # парсинг счёта
+    def __parsing_score(self, s: str) -> list:
+        return list(map(int, s[:3].split(":")))
 
     def __count_games(self):
         # возвращается таблица с кол-вом игр
@@ -126,9 +121,71 @@ class Football:
         # вычисление итоговых очков: на 1 победу - +3 очка, на ничью - +1 очко
         return DataFrame({"О": game_scores["В"] * 3 + game_scores["Н"]})
 
+    def __private_meetings(self, table: DataFrame):
+        # датафрейм, который показывает частоту вхождений очков команд
+        scores = table['О'].value_counts()
 
-# filename = input("Введите имя файла (пример: 15-16): ")
-filename = argv[1]
+        # датафрейм с вхождениями очков больше 1
+        scores = scores[scores > 1]
+
+        # датафрейм, в который входят команды с одинаковыми очками
+        recurring_team_points = table[table["О"].isin(scores.index.to_list())]
+
+        # достаем команды из первичной таблицы
+        raw_data = self.data[
+            (self.data["Команда_1"].isin(recurring_team_points["Команда"])) &
+            (self.data["Команда_2"].isin(recurring_team_points["Команда"]))
+        ]
+
+        # удаление столбцов "стадион", "Дата", "Время"
+        raw_data.drop(columns=["стадион", "Дата", "Время"])
+
+        # список, в который собираются части датафреймов
+        private_matches = []
+        # список со списками индексов и очков
+        index_and_points = [[index, column["О"]] for index, column in recurring_team_points.iterrows()]
+
+        for first, second in itertools.product(range(len(index_and_points) - 1), range(1, len(index_and_points))):
+            if index_and_points[first][1] == index_and_points[second][1] and first != second:
+                team_1 = recurring_team_points.loc[index_and_points[first][0]]["Команда"]
+                team_2 = recurring_team_points.loc[index_and_points[second][0]]["Команда"]
+
+                meetings = raw_data[
+                    (raw_data["Команда_1"].isin([team_1, team_2])) & (raw_data["Команда_2"].isin([team_1, team_2]))
+                ]
+                private_matches.append(meetings)
+
+        # объединение частей датафреймов и удаление дубликатов
+        private_matches = pd.concat(private_matches).drop_duplicates()
+
+        # парсинг счета из таблицы final
+        meeting_scores = private_matches["счёт"].apply(self.__parsing_score)
+        # подсчет кол-ва выигрышей ничьих и поражений
+        meeting_game_scores = self.__calculate_game_scores(
+            tables=[private_matches["Команда_1"], private_matches["Команда_2"]], scores=meeting_scores
+        )
+        # подсчет кол-ва забитых голов
+        meeting_goals = self.__calculate_goals(
+            tables=[private_matches["Команда_1"], private_matches["Команда_2"]], scores=meeting_scores
+        )
+        # подсчет разницы забитых и пропущенных голов
+        goal_difference = meeting_goals["ЗГ"] - meeting_goals["ПГ"]
+
+        # сортировка по забитым мячам и разности между забитыми и пропущенными
+        teams_meetings_table = meeting_goals.join([meeting_game_scores, goal_difference]).rename(columns={
+            0: "Разн",
+            "В": "Выигр",
+            "ЗГ": "Заб",
+        }).sort_values(by=["Заб", "Разн"], ascending=False).reset_index()
+
+        # удаляем ненужные столбцы
+        teams_meetings_table.drop(columns=["ПГ", "Н", "П"], axis=1, inplace=True)
+
+        return teams_meetings_table
+
+
+filename = input("Введите имя файла (пример: 15-16): ")
+# filename = argv[1]
 # filename = "18-19"
 path = f"csv\\rpl\\{filename}.csv"
 
